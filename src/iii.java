ad1689c07147f409763e909b9cf73165f58e67aa
kdg <grandstand7@gmail.com>
2024-05-10 00:01:47 +0900

    주석 추가

--- src/iii.java
+++ src/iii.java
@@ -1,8 +1,10 @@
-
+/*
+ * iii 클래스란
+ */
 public class iii {
 
 	public static void main(String[] args) {
-		
+		// 오키바리
 		for(int i =0 ; i < 10; i++)
 			System.out.println("== hello world!!!");
 		
