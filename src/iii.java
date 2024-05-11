850cb1dd21af8d84b2c65daf868007d8d5c72d37
kdg <grandstand7@gmail.com>
2024-05-11 15:37:24 +0900

    iii.java 수정후 올림

--- src/iii.java
+++ src/iii.java
@@ -8,8 +8,7 @@ public class iii {
 		for(int i =0 ; i < 10; i++)
 			System.out.println("== hello world!!!");
 		
-		System.out.println("11111111111111111111111");
-		System.out.println("22222222222222222222222222222");
+		System.out.println("hello world===");
 		
 	}
 
