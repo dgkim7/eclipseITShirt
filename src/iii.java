84510f87433cd13dc97f28f76da78ae5bbe6dfb3
kdg <grandstand7@gmail.com>
2024-05-11 16:06:05 +0900

    11111

--- src/iii.java
+++ src/iii.java
@@ -8,7 +8,7 @@ public class iii {
 		for(int i =0 ; i < 10; i++)
 			System.out.println("== hello world!!!");
 		
-		System.out.println("5555555555555555");
+		System.out.println("11111111111111");
 	}
 
 }
